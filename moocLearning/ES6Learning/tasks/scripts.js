de05b95640fc3b265e117d254ceb52e478847aae
CinCommon <yinzifancn@gmail.com>
2018-01-05 00:09:01 +0800

    -- yinzf2

---
+++ moocLearning/ES6Learning/tasks/scripts.js
@@ -0,0 +1,42 @@
+import gulp from 'gulp';
+import gulpif from 'gulp-if'; // gulp语句中做if判断
+import concat from 'gulp-concat'; // gulp处理文件拼接
+import webpack from 'webpack'; // 打包
+import gulpWebpack from 'webpack-stream'; // 处理文件流
+import named from 'vinyl-named'; // 对文件重命名的标志
+import livereload from 'gulp-livereload'; // 热更新
+import plumber from 'gulp-plumber'; // 处理文件信息流
+import rename from 'gulp-rename'; // 处理文件重命名
+import uglify from 'gulp-uglify'; // 处理js和css压缩
+import {log, colors} from 'gulp-util'; // 在命令行工具输出的工具包
+import args from './util/args'; // 对命令行参数进行解析
+
+gulp.task('scripts', ()=>{
+    return gulp.src(['app/js/index.js'])
+    .pipe(plumber({
+        errorHandle:function(){
+
+        }
+    }))
+    .pipe(named())
+    .pipe(gulpWebpack({
+        module:{
+            loaders: [{
+                test:/\.js$/,
+                loader: 'babel'
+            }]
+        }
+    }),null,(err,stats)=>{
+        log(`Finished '${colors.cyan('scripts')}'`,stats.toString({
+            chunks:false
+        }))
+    })
+    .pipe(gulp.dest('server/public/js'))
+    .pipe(rename({
+        basename: 'cp',
+        extname: '.min.js'
+    }))
+    .pipe(uglify({compress: {properties: false}, output: {'quote_keys': true}}))
+    .pipe(gulp.dest('server/public/js'))
+    .pipe(gulpif(args.watch, livereload()))
+})
---
+++ moocLearning/ES6Learning/tasks/util/args.js
@@ -0,0 +1,32 @@
+import yargs from 'yargs';
+
+// 区分开发环境和线上环境
+const args = yargs
+
+    .option('production', {
+        boolean: true,
+        default: false, // 设置默认值
+        describe: 'min all script' // 描述
+    })
+    //监听要不要自动编译
+    .option('watch', {
+        boolean: true,
+        default: false,
+        describe: 'watch all files'
+    })
+    // 要不要详细的输出命令行输出的日志
+    .option('verbose', {
+        boolean: true,
+        default: false,
+        describe: 'log'
+    })
+    // 资源映射
+    .option('sourcemaps', { describe: 'force the creation of sourcemap' })
+    // 设置端口
+    .option('port', {
+        string: true,
+        default: 8080,
+        describe: 'server port'
+    })
+    
+    .argv // 命令行输入的内容以字符串作为解析
