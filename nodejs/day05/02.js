05902d95bc71432d895dd2c9bc6726efe3610762
CinCommon <452495748@qq.com>
2018-03-05 23:12:31 +0800

    ''

--- nodejs/day05/02.js
+++ nodejs/day05/02.js
@@ -4,14 +4,22 @@ var app = express();
 var db = require('./model/db.js')
 
 app.get('/insertOne', function(req, res){
-    db.insertOne('person', 'student', {'name': 'student01'}, function(err, result){
-        if(err) {
-            console.log('插入失败')
-            throw err
-        }
-        res.send("插入成功!!")
-    })
+    for(var i = 8; i<=100; i++){
+        db.insertOne('person', 'student', {'name': 'student0'+i}, function(err, result){
+            if(err) {
+                console.log('插入失败')
+                throw err
+            }
+            // res.send("插入成功!!")
+        })
+    }
 
+})
+app.get('/find', function(req, res){
+    var page = req.query.page
+    db.find('person', 'student', {}, function(err, result){
+        res.json({"result":result})
+    })
 })
 
 
--- nodejs/day05/model/db.js
+++ nodejs/day05/model/db.js
@@ -1,8 +1,8 @@
 var mongoClient = require('mongodb').MongoClient;
 
 function _connect(callback) {
-    mongoClient.connect('mongodb://localhost:27017/',function(err, client){
-        if(err) {
+    mongoClient.connect('mongodb://localhost:27017/', function (err, client) {
+        if (err) {
             console.log(err)
             throw err
         }
@@ -12,15 +12,48 @@ function _connect(callback) {
 }
 
 exports.insertOne = function (dbName, collectionName, json, callback) {
-    _connect(function(err, client){
+    _connect(function (err, client) {
         var db = client.db(dbName)
-        db.collection(collectionName).insertOne(json, function(err, result){
+        db.collection(collectionName).insertOne(json, function (err, result) {
             callback(err, result)
             client.close();
         })
     })
 }
 
-exports.find = function (dbName, collectionName, json, args, callback) {
-    
+exports.find = find
+
+// function find(dbName, collectionName, json, callback) {
+//     find(dbName,collectionName,json,{},callback)
+// }
+
+function find(dbName, collectionName, json, callback, limit_skip) {
+    var result = []
+    var limit, skip
+    console.log(arguments)
+    if (arguments.length != 5) {
+        limit = 0
+        skip = 0
+        // callback('arguments`s length must be 5!', null)
+    }
+
+    // console.log(typeof limit_skip == )
+    json = json || {}
+    _connect(function (err, client) {
+        if (err) {
+            callback(err, null)
+        }
+        var db = client.db(dbName)
+        var cursor = db.collection(collectionName).find(json)
+        cursor.each(function (err, doc) {
+            if(err){
+                callback('', null)
+            }
+            if (doc) {
+                result.push(doc)
+            } else {
+                callback(null, result)
+            }
+        })
+    })
 }
